04eb7593dd0809b03b15eaea4ba46976c8ca23f7
j-gens <jgens2@gmail.com>
2019-10-21 18:20:49 -0700

    Boilerplate express server

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express require('express');
+const express = require('express');
 
 const app = express();
 const port = 3000;
 
-app.static(express.static('public'));
+app.use(express.static('public'));
 
 app.listen(port, () => console.log(`proxy server on ${port} is here!`));
