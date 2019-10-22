42e11c02cf4eb21c305e7c8c20a261c13c033d7c
j-gens <jgens2@gmail.com>
2019-10-22 08:34:04 -0700

    Update server to route requests to other ports

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const proxy = require('express-http-proxy');
 
 const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+
+app.use('/', proxy('localhost:3100'));
+
 
 app.listen(port, () => console.log(`proxy server on ${port} is here!`));
